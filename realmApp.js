d2435840c44cdc63639aead8c72fbc8754b2f854
SNT <chetank>
2022-04-19 17:33:57 +0530

    initial commit

---
+++ realmApp.js
@@ -0,0 +1,10 @@
+import Realm from "realm";
+
+
+// Invokes the shared instance of the Realm app.
+// TODO: Create a Realm App instance with your Realm app ID.
+// const app = new Realm.App({id: "<your Realm app ID here>"}); // Set Realm app ID here.
+const app = new Realm.App({id: "to-do-udwrf"}); // Set Realm app ID here.
+
+export default app;
+
